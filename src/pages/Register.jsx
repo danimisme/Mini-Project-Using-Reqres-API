007e0fd4f94c6905b1be26196595a9dbed21d82a
danimisme <msubhanr53@gmail.com>
2024-02-27 01:03:23 +0700

    import axios for handling HTTP request

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import InputForm from "../components/Elements/Input/InputForm";
 import { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
   const [token, setToken] = useState(undefined);
