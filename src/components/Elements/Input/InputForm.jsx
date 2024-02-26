bec515aa5226b960e8771a95e8c4ac65cea30c57
danimisme <msubhanr53@gmail.com>
2024-02-26 23:29:02 +0700

    Refactoring InputForm, use reusable Label component

--- src/components/Elements/Input/InputForm.jsx
+++ src/components/Elements/Input/InputForm.jsx
@@ -1,8 +1,10 @@
+import Label from "./Label";
+
 const InputForm = (props) => {
   const { name, label, type, placeholder } = props;
   return (
     <div className="input-container">
-      <label htmlFor={name}>{label}</label>
+      <Label name={name} label={label} />
       <input type={type} placeholder={placeholder} id={name} />
     </div>
   );
