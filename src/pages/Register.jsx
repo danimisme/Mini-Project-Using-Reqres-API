f83a3f6bc6ca528f6a0bb9b551cdd4ae36e24526
danimisme <msubhanr53@gmail.com>
2024-02-27 00:46:31 +0700

    create Register Page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import InputForm from "../components/Elements/Input/InputForm";
+
+const Register = () => {
+  return (
+    <div className="form-container">
+      <h1>Register</h1>
+      <form>
+        <InputForm
+          name="email"
+          label="Email"
+          type="email"
+          placeholder="Enter Your Email ..."
+        />
+        <InputForm
+          name="password"
+          label="Password"
+          type="password"
+          placeholder="Enter Your Password ..."
+        />
+        <button type="submit">Login</button>
+        <p>
+          Don't have an account? <Link to="/login">Login</Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
