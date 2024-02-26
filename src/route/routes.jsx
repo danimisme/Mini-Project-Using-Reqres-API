4d0de8b5c9b9767d23ec4329e7a8555869e6d2b4
danimisme <msubhanr53@gmail.com>
2024-02-27 00:47:47 +0700

    add path /register in routeList

--- src/route/routes.jsx
+++ src/route/routes.jsx
@@ -1,8 +1,13 @@
 import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 export const routeList = [
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
 ];
