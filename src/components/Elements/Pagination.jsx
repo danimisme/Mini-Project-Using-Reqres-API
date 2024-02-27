9f893c3b1f5261afdb8d41f6c4d0544c34df79b3
danimisme <msubhanr53@gmail.com>
2024-02-28 00:10:17 +0700

    modify Pagination component to dinamicaly handle page count

--- src/components/Elements/Pagination.jsx
+++ src/components/Elements/Pagination.jsx
@@ -1,5 +1,4 @@
-const Pagination = ({ page, setPage }) => {
-  const pages = 2;
+const Pagination = ({ page, pages, setPage }) => {
   const pageNumbers = [];
   for (let i = 1; i <= pages; i++) {
     pageNumbers.push(i);
