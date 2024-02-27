25f9049531e348fa63d49ff4dd8c8a5dffaa39f7
danimisme <msubhanr53@gmail.com>
2024-02-27 23:22:58 +0700

    create user state

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,7 +1,11 @@
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 const User = () => {
   let { id } = useParams();
+
+  const [user, setUser] = useState({});
+
   return (
     <div className="container-lg">
       <h1>User : {id}</h1>
