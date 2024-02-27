20f3c66ac270b8bcb949d7eb5c7aaa6ae4658619
danimisme <msubhanr53@gmail.com>
2024-02-27 20:56:28 +0700

    create listUser state with useState

--- src/pages/ListUser.jsx
+++ src/pages/ListUser.jsx
@@ -1,6 +1,9 @@
 import axios from "axios";
+import { useState } from "react";
 
 const ListUser = () => {
+  const [listUser, setListUser] = useState([]);
+
   return (
     <div className="container-lg">
       <h1>Hello ReqRes Users !</h1>
