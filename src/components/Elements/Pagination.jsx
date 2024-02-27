6b18b4add33010dd804818718c6ff298a728232a
danimisme <msubhanr53@gmail.com>
2024-02-27 22:28:21 +0700

    create Pagiation element for navigate listUsers page

---
+++ src/components/Elements/Pagination.jsx
@@ -0,0 +1,34 @@
+const Pagination = ({ page, setPage }) => {
+  const pages = 2;
+  const pageNumbers = [];
+  for (let i = 1; i <= pages; i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav aria-label="...">
+      <ul className="pagination">
+        <li className="page-item disabled">
+          <a className="page-link">Previous</a>
+        </li>
+        {pageNumbers.map((number) => (
+          <li key={number} className="page-item">
+            <p
+              className={`page-link ${page === number ? "active" : ""}`}
+              onClick={() => setPage(number)}
+            >
+              {number}
+            </p>
+          </li>
+        ))}
+        <li className="page-item">
+          <a className="page-link" href="#">
+            Next
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
