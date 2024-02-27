db7eb6c1f7d4b435ca7ed09a61126321f5f97556
danimisme <msubhanr53@gmail.com>
2024-02-27 20:50:49 +0700

    add route '/' for element ListUser.jsx

--- src/route/routes.jsx
+++ src/route/routes.jsx
@@ -1,7 +1,12 @@
+import ListUser from "../pages/ListUser";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 
 export const routeList = [
+  {
+    path: "/",
+    element: <ListUser />,
+  },
   {
     path: "/login",
     element: <Login />,
