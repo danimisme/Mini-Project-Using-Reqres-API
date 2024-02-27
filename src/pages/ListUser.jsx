0a1640212035630c969eab4cda909d6415915c76
danimisme <msubhanr53@gmail.com>
2024-02-27 21:59:58 +0700

    add listUsers.map for renderig user cards

--- src/pages/ListUser.jsx
+++ src/pages/ListUser.jsx
@@ -21,7 +21,26 @@ const ListUser = () => {
 
   return (
     <div className="container-lg">
-      <h1>Hello ReqRes Users !</h1>
+      <h1 className="text-center">Hello ReqRes Users !</h1>
+      <div className="row  justify-content-center gap-1">
+        {listUsers.map((user) => (
+          <div
+            className="card col-10 col-md-4 col-lg-3 my-3  border-0"
+            key={user.id}
+          >
+            <div className="image-container">
+              <img
+                src={user.avatar}
+                className="card-img-top img-fluid"
+                alt="..."
+              />
+            </div>
+            <div className="card-body text-center">
+              <p className="card-text fw-bold fs-4">{user.first_name}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
