cf1fdf6af254b674d89e56b227bc7a3e018f49e2
danimisme <msubhanr53@gmail.com>
2024-02-28 22:37:33 +0700

    add navbar component into List User Page

--- src/pages/ListUser.jsx
+++ src/pages/ListUser.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Pagination from "../components/Elements/Pagination";
 import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const ListUser = () => {
   const [listUsers, setListUsers] = useState([]);
@@ -23,34 +24,37 @@ const ListUser = () => {
   }, [page]);
 
   return (
-    <div className="container-lg">
-      <h1 className="text-center">Hello ReqRes Users !</h1>
-      <div className="row  justify-content-center gap-1">
-        {listUsers.map((user) => (
-          <div
-            className="card col-10 col-md-4 col-lg-3 my-3  border-0"
-            key={user.id}
-          >
-            <Link
-              to={`/user/${user.id}`}
-              style={{ textDecoration: "none", color: "black" }}
+    <>
+      <Navbar active={"List User"} />
+      <div className="container-lg">
+        <h1 className="text-center">Hello ReqRes Users !</h1>
+        <div className="row  justify-content-center gap-1">
+          {listUsers.map((user) => (
+            <div
+              className="card col-10 col-md-4 col-lg-3 my-3  border-0"
+              key={user.id}
             >
-              <div className="image-container">
-                <img
-                  src={user.avatar}
-                  className="card-img-top img-fluid"
-                  alt="..."
-                />
-              </div>
-              <div className="card-body text-center">
-                <p className="card-text fw-bold fs-4">{user.first_name}</p>
-              </div>
-            </Link>
-          </div>
-        ))}
+              <Link
+                to={`/user/${user.id}`}
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                <div className="image-container">
+                  <img
+                    src={user.avatar}
+                    className="card-img-top img-fluid"
+                    alt="..."
+                  />
+                </div>
+                <div className="card-body text-center">
+                  <p className="card-text fw-bold fs-4">{user.first_name}</p>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+        <Pagination page={page} setPage={setPage} pages={2} />
       </div>
-      <Pagination page={page} setPage={setPage} pages={2} />
-    </div>
+    </>
   );
 };
 
