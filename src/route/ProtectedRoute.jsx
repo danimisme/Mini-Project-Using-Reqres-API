1ee77ab2ea4d18ce97df5370e595d1330d028382
danimisme <msubhanr53@gmail.com>
2024-02-29 00:06:34 +0700

    Create Protected Route

---
+++ src/route/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    return <Navigate to={"/login"} />;
+  } else if (token) {
+    return <Navigate to={"/"} />;
+  }
+  return children;
+};
+
+export default ProtectedRoute;
