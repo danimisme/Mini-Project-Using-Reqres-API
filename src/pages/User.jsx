498446937db87123a15467c0da34f8b83acefd53
danimisme <msubhanr53@gmail.com>
2024-02-28 22:41:17 +0700

    add navbar component into User Page

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -2,6 +2,7 @@ import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Pagination from "../components/Elements/Pagination";
+import Navbar from "../components/Navbar";
 
 const User = () => {
   let { id } = useParams();
@@ -25,36 +26,41 @@ const User = () => {
   }, [page]);
 
   return (
-    <div
-      className="container-lg mt-5 d-flex flex-column "
-      style={{ height: "80vh" }}
-    >
-      <div className="row justify-content-center gap-1">
-        <div className="col-8 col-md-3 col-lg-4">
-          <img
-            src={user.avatar}
-            className="img-fluid w-100 rounded"
-            alt="avatar"
-          />
+    <>
+      <Navbar active={"User"} />
+      <div
+        className="container-lg mt-5 d-flex flex-column "
+        style={{ height: "80vh" }}
+      >
+        <div className="row justify-content-center gap-1">
+          <div className="col-8 col-md-3 col-lg-4">
+            <img
+              src={user.avatar}
+              className="img-fluid w-100 rounded"
+              alt="avatar"
+            />
+          </div>
+          <div className="col-10 col-md-4 col-lg-5 align-self-center">
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">ID : {user.id}</li>
+              <li className="list-group-item">
+                First Name : {user.first_name}
+              </li>
+              <li className="list-group-item">Last Name : {user.last_name}</li>
+              <li className="list-group-item">Email : {user.email}</li>
+            </ul>
+          </div>
         </div>
-        <div className="col-10 col-md-4 col-lg-5 align-self-center">
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">ID : {user.id}</li>
-            <li className="list-group-item">First Name : {user.first_name}</li>
-            <li className="list-group-item">Last Name : {user.last_name}</li>
-            <li className="list-group-item">Email : {user.email}</li>
-          </ul>
+        <div className="button-container d-flex justify-content-end col-10">
+          <Link to={`/`} className="btn btn-dark">
+            Back
+          </Link>
+        </div>
+        <div className="mt-auto">
+          <Pagination pages={12} page={page} setPage={setPage} />
         </div>
       </div>
-      <div className="button-container d-flex justify-content-end col-10">
-        <Link to={`/`} className="btn btn-dark">
-          Back
-        </Link>
-      </div>
-      <div className="mt-auto">
-        <Pagination pages={12} page={page} setPage={setPage} />
-      </div>
-    </div>
+    </>
   );
 };
 
