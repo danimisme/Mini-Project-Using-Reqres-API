c44080726e44b185816ced179fc79918e5ca9775
danimisme <msubhanr53@gmail.com>
2024-02-28 22:36:01 +0700

    add props to change style to add class active on active navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = ({ active }) => {
                 List User
               </a>
             </li>
-            <li className="nav-item">
+            <li className={`nav-item ${active === "User" && "active"}`}>
               <a className="nav-link" href="#">
                 User Profile
               </a>
