06ca448659ba4d11129d906416cc179e45f44112
danimisme <msubhanr53@gmail.com>
2024-02-29 21:05:15 +0700

    change data aos offset

--- aos.js
+++ aos.js
@@ -14,7 +14,7 @@ function Animation() {
     throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-    offset: 200, // offset (in px) from the original trigger point
+    offset: 120, // offset (in px) from the original trigger point
     delay: 0, // values from 0 to 3000, with step 50ms
     duration: 1000, // values from 0 to 3000, with step 50ms
     easing: "ease", // default easing for AOS animations
