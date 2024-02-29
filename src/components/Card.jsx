5423153e643e7df2a4beda2869f29100845e0fa4
danimisme <msubhanr53@gmail.com>
2024-02-29 21:05:45 +0700

    add animation use aos for card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,18 @@
 import { Link } from "react-router-dom";
+import Animation from "../../aos";
+import { useEffect } from "react";
 
 const Card = (props) => {
+  useEffect(() => {
+    Animation();
+  }, []);
   const { avatar, id, firstName, email } = props;
   return (
-    <div className="card col-10 col-md-4 col-lg-3 my-3 mx-2">
+    <div
+      className="card col-10 col-md-4 col-lg-3 my-3 mx-2"
+      data-aos="fade-up"
+      data-aos-delay={`${(id % 7) * 100}`}
+    >
       <Link
         to={`/user/${id}`}
         style={{ textDecoration: "none", color: "black", margin: "0" }}
