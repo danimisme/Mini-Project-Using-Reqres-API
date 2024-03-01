e42768396984aeee96236badc5d898ddb7780250
danimisme <msubhanr53@gmail.com>
2024-03-01 23:14:09 +0700

    Create ImageContainer Element

---
+++ src/components/Elements/ImageContainer.jsx
@@ -0,0 +1,16 @@
+import Animation from "../../../aos";
+import { useEffect } from "react";
+
+const ImageContainer = (props) => {
+  useEffect(() => {
+    Animation();
+  }, []);
+  const { src, alt = "image" } = props;
+  return (
+    <div className=" col-4 d-none d-lg-block" style={{ zIndex: "1" }}>
+      <img src={src} alt={alt} className="img-fluid" data-aos="fade-right" />
+    </div>
+  );
+};
+
+export default ImageContainer;
