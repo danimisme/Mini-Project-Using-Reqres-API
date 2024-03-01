503d6be5039d109aca741d73f217d0b41eb66b6a
danimisme <msubhanr53@gmail.com>
2024-03-01 23:44:21 +0700

    refactoring Login Page, add LoginForm Components

---
+++ src/components/Fragments/LoginForm.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+import { loginProcess } from "../../utils/apiUtils";
+import InputForm from "../Elements/Input/InputForm";
+import PasswordField from "../Elements/PasswordField";
+import { useEffect } from "react";
+import Animation from "../../../aos";
+
+const LoginForm = () => {
+  useEffect(() => {
+    Animation();
+  });
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const data = {
+      email: e.target.email.value,
+      password: e.target.password.value,
+    };
+    loginProcess(data);
+  };
+  return (
+    <div className="form-container" data-aos="fade-down">
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <InputForm
+          name="email"
+          label="Email"
+          type="email"
+          placeholder="Enter Your Email ..."
+        />
+        <PasswordField
+          name="password"
+          label="Password"
+          placeholder="Enter Your Password ..."
+        />
+        <button type="submit">Login</button>
+        <p>
+          Don't have an account? <Link to="/register">Register</Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,51 +1,14 @@
-import { useEffect } from "react";
-import InputForm from "../components/Elements/Input/InputForm";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Link } from "react-router-dom";
-import PasswordField from "../components/Elements/PasswordField";
 import WaveHeader from "../components/Fragments/WaveHeader";
-import Animation from "../../aos";
-import { loginProcess } from "../utils/apiUtils";
 import ImageContainer from "../components/Elements/ImageContainer";
+import LoginForm from "../components/Fragments/LoginForm";
 const Login = () => {
-  useEffect(() => {
-    Animation();
-  }, []);
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-    const data = {
-      email: e.target.email.value,
-      password: e.target.password.value,
-    };
-    loginProcess(data);
-  };
-
   return (
     <>
       <WaveHeader>
         <ImageContainer src="./image/login-image.png" alt="image" />
-        <div className="form-container" data-aos="fade-down">
-          <h1>Login</h1>
-          <form onSubmit={handleLogin}>
-            <InputForm
-              name="email"
-              label="Email"
-              type="email"
-              placeholder="Enter Your Email ..."
-            />
-            <PasswordField
-              name="password"
-              label="Password"
-              placeholder="Enter Your Password ..."
-            />
-            <button type="submit">Login</button>
-            <p>
-              Don't have an account? <Link to="/register">Register</Link>
-            </p>
-          </form>
-        </div>
+        <LoginForm />
       </WaveHeader>
       <ToastContainer position="top-center" theme="dark" autoClose={1000} />
     </>
