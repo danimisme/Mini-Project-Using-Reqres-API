c4130b6a20719316384f4a99fee16eaaaf5eeb8e
danimisme <msubhanr53@gmail.com>
2024-03-03 21:58:04 +0700

    refactoring User page, use Layout component

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,9 +1,10 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Pagination from "../components/Fragments/Pagination";
 import Animation from "../../aos";
 import { getUser } from "../utils/apiUtils";
 import UserDetailCard from "../components/Fragments/UserDetailCard";
+import Layout from "../components/Layouts/Layout";
 const User = () => {
   let { id } = useParams();
   const [user, setUser] = useState({});
@@ -15,15 +16,14 @@ const User = () => {
   }, [page]);
 
   return (
-    <>
-      <Navbar active={"User"} />
+    <Layout navActive="User">
       <div className=" user-container container-lg mt-5">
         <UserDetailCard {...user} />
         <div className="mt-auto">
           <Pagination pages={12} page={page} setPage={setPage} />
         </div>
       </div>
-    </>
+    </Layout>
   );
 };
 
