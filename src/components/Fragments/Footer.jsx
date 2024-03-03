e254609ebc6d42a422bf7acde4e19515835bd984
danimisme <msubhanr53@gmail.com>
2024-03-03 17:38:22 +0700

    create Footer Component

---
+++ src/components/Fragments/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className=" bg-dark text-white text-center p-3">
+      <p className="m-0 fs-6">
+        Dibimbing.id | Bootcamp Front End Web Developer Batch 16 <br /> &copy;
+        Copyright 2024 Muhammad Subhan Ramdhani
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
