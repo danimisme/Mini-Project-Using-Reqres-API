5c1ae9f31016a9209e0f8ebac8fc775ecb8caf95
danimisme <msubhanr53@gmail.com>
2024-03-03 09:54:35 +0700

    refactoring user page, create reusable userDetailCard component

---
+++ src/components/Fragments/UserDetailCard.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+
+const UserDetailCard = (props) => {
+  const { first_name, last_name, email, avatar, id } = props;
+  return (
+    <>
+      <div className="row justify-content-center gap-1">
+        <div className="col-8 col-md-3 col-lg-4">
+          <img
+            src={avatar}
+            className="img-fluid w-100 rounded"
+            alt="avatar"
+            data-aos="fade-right"
+          />
+        </div>
+        <div
+          className="col-10 col-md-4 col-lg-5 align-self-center"
+          data-aos="fade-left"
+        >
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">ID : {id}</li>
+            <li className="list-group-item">First Name : {first_name}</li>
+            <li className="list-group-item">Last Name : {last_name}</li>
+            <li className="list-group-item">Email : {email}</li>
+          </ul>
+        </div>
+      </div>
+      <div className="button-container d-flex justify-content-end col-10">
+        <Link to={`/`} className="btn btn-dark">
+          Back
+        </Link>
+      </div>
+    </>
+  );
+};
+export default UserDetailCard;
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,6 +4,7 @@ import Navbar from "../components/Fragments/Navbar";
 import Pagination from "../components/Fragments/Pagination";
 import Animation from "../../aos";
 import { getUser } from "../utils/apiUtils";
+import UserDetailCard from "../components/Fragments/UserDetailCard";
 const User = () => {
   let { id } = useParams();
   const [user, setUser] = useState({});
@@ -21,34 +22,7 @@ const User = () => {
         className="container-lg mt-5 d-flex flex-column "
         style={{ height: "80vh" }}
       >
-        <div className="row justify-content-center gap-1">
-          <div className="col-8 col-md-3 col-lg-4">
-            <img
-              src={user.avatar}
-              className="img-fluid w-100 rounded"
-              alt="avatar"
-              data-aos="fade-right"
-            />
-          </div>
-          <div
-            className="col-10 col-md-4 col-lg-5 align-self-center"
-            data-aos="fade-left"
-          >
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">ID : {user.id}</li>
-              <li className="list-group-item">
-                First Name : {user.first_name}
-              </li>
-              <li className="list-group-item">Last Name : {user.last_name}</li>
-              <li className="list-group-item">Email : {user.email}</li>
-            </ul>
-          </div>
-        </div>
-        <div className="button-container d-flex justify-content-end col-10">
-          <Link to={`/`} className="btn btn-dark">
-            Back
-          </Link>
-        </div>
+        <UserDetailCard {...user} />
         <div className="mt-auto">
           <Pagination pages={12} page={page} setPage={setPage} />
         </div>
