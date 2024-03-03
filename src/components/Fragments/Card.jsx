e97c75656dc552381f2fa027090373727f5b5b8f
danimisme <msubhanr53@gmail.com>
2024-03-03 18:07:16 +0700

    add card style if darkMode = true

--- src/components/Fragments/Card.jsx
+++ src/components/Fragments/Card.jsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
 import Animation from "../../../aos";
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
+import { DarkMode } from "../../context/DarkMode";
 
 const Card = (props) => {
+  const { avatar, id, first_name, email } = props;
+  const { isDarkMode, setDarkMode } = useContext(DarkMode);
   useEffect(() => {
     Animation();
-  }, []);
-  const { avatar, id, first_name, email } = props;
+  }, [isDarkMode]);
   return (
     <div
-      className="card col-10 col-md-4 col-lg-3 my-3 mx-2"
+      className={`card col-10 col-md-4 col-lg-3 my-3 mx-2 ${
+        isDarkMode && " dark-card"
+      }`}
       data-aos="fade-up"
       data-aos-delay={`${(id % 7) * 100}`}
     >
@@ -21,7 +25,7 @@ const Card = (props) => {
           <img src={avatar} className="card-img-top img-fluid" alt="..." />
         </div>
         <div className="card-body text-center ">
-          <p className="card-text fw-bold fs-4 border-2 border-bottom">
+          <p className="card-text fw-semibold fs-4 border-2 border-bottom">
             {first_name}
           </p>
           <p className="card-text"> {email}</p>
