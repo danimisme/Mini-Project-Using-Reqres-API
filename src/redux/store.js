f16b279075168fd55d3ff959b187c79868885da6
danimisme <msubhanr53@gmail.com>
2024-03-11 01:37:15 +0700

    add modalShowReducer to store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { combineReducers, createStore } from "redux";
 import darkModeReducers from "./reducers/darkModeReducers";
+import modalShowReducer from "./reducers/modalShowReducer";
 
 const rootReducer = combineReducers({
   darkMode: darkModeReducers,
+  modalShow: modalShowReducer,
 });
 
 const store = createStore(rootReducer);
