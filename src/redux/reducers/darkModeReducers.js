c34cfc7402a5bd4328420d84e14809ac48f525c6
danimisme <msubhanr53@gmail.com>
2024-03-12 01:48:14 +0700

    save isDarkMode value to sessionStorage

--- src/redux/reducers/darkModeReducers.js
+++ src/redux/reducers/darkModeReducers.js
@@ -1,10 +1,11 @@
 const initialState = {
-  isDarkMode: false,
+  isDarkMode: sessionStorage.getItem("isDarkMode") === "true" ? true : false,
 };
 
 const darkModeReducers = (state = initialState, action) => {
   switch (action.type) {
     case "darkMode/setDarkMode":
+      sessionStorage.setItem("isDarkMode", !state.isDarkMode);
       return { ...state, isDarkMode: !state.isDarkMode };
     default:
       return state;
